70bdf44190ee045b0d85d763c6433310d3e5bc3c
Tarek376 <tarek.semhan2003@gmail.com>
2023-11-27 22:14:13 +0200

    Sec commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            primary: const Color.fromARGB(255, 7, 19, 89),
-            padding: EdgeInsets.symmetric(vertical: 14, horizontal: 40),
+            primary: Colors.indigo,
+            padding: EdgeInsets.symmetric(vertical: 14, horizontal: 16),
           ),
         ),
       ),
@@ -42,52 +42,58 @@ class _NameInputPageState extends State<NameInputPage> {
   TextEditingController _nameController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFB8B8E2),
-      body:Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXt8cvr8fzEfvR07rX3K-i2ChbzAJyuKlh-thK1baheEjblFqSSZY9eG8qZAsawe2ypBo&usqp=CAU'), // Replace 'your_image.jpg' with your actual image path
-            fit: BoxFit.cover,
-          ),
-        ),
-          child:Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 200,
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Enter your name',
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            MyHome(username: _nameController.text),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Submit',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ],
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF5731BF),
+        title: const Center(
+          child: Text(
+            'Quizzy App',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 200,
+              child: Image.network(
+                'https://imgs.search.brave.com/QrHlJhtAo76xCRt_x06SLpYUQJTg5FR6lcA8AHplrq8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdDIu/ZGVwb3NpdHBob3Rv/cy5jb20vMTAwMjI3/Ny81OTc5L2kvNjAw/L2RlcG9zaXRwaG90/b3NfNTk3OTI1ODEt/c3RvY2stcGhvdG8t/cXVpei1jdWJpY3Mu/anBn',
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                labelText: 'Enter your name',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => MyHome(username: _nameController.text),
+                  ),
+                );
+              },
+              child: const Text(
+                'Submit',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
